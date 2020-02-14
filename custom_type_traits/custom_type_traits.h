402d044a79cfd0a1ae758532e6bed0a23300f689
Makerslet <oshuev.valery@yandex.ru>
2020-02-14 12:38:31 +0300

    is_same, is_void, is_null_pointer

--- custom_type_traits/custom_type_traits.h
+++ custom_type_traits/custom_type_traits.h
@@ -1,4 +1,30 @@
 #ifndef CUSTOM_TYPE_TRAITS_H
 #define CUSTOM_TYPE_TRAITS_H
 
+#include <cstddef>
+
+namespace custom_tt {
+
+struct value_false {
+    static constexpr bool value = false;
+};
+
+struct value_true {
+    static constexpr bool value = true;
+};
+
+template <typename T1, typename T2>
+struct is_same : value_false {};
+
+template <typename T>
+struct is_same<T, T> : value_true {};
+
+template <typename T>
+struct is_void : is_same<T, void> {};
+
+template <typename T>
+struct is_null_pointer : is_same<T, std::nullptr_t> {};
+
+}
+
 #endif // CUSTOM_TYPE_TRAITS_H
--- custom_type_traits/main.cpp
+++ custom_type_traits/main.cpp
@@ -1,7 +1,30 @@
 #include <iostream>
 #include "custom_type_traits.h"
 
+using namespace custom_tt;
+
 int main(int argc, char * argv[]) {
+
+    (void) argc;
+    (void) argv;
+
+    // is_same
+    std::cout << "is_same<int, int>: "          << is_same<int, int>::value             << std::endl;
+    std::cout << "is_same<int, std::int64_t>: " << is_same<int, std::int64_t>::value    << std::endl;
+    std::cout << "is_same<int, std::int32_t>: " << is_same<int, std::int32_t>::value    << std::endl;
+    std::cout << std::endl;
+
+    // is_void
+    std::cout << "is_void<int>: "       << is_void<int>::value              << std::endl;
+    std::cout << "is_void<void>: "      << is_void<void>::value             << std::endl;
+    std::cout << "is_void<nullptr_t>: " << is_void<std::nullptr_t>::value   << std::endl;
+    std::cout << std::endl;
+
+    // is_void
+    std::cout << "is_null_pointer<int*>: "              << is_null_pointer<int*>::value             << std::endl;
+    std::cout << "is_null_pointer<void*>: "             << is_null_pointer<void*>::value            << std::endl;
+    std::cout << "is_null_pointer<std::nullptr_t>: "    << is_null_pointer<std::nullptr_t>::value   << std::endl;
+    std::cout << std::endl;
 
     return 0;
 }
